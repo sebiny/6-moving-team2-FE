60594dfbc5f5af22db9d8d77f4ec163b095679aa
Sue <sue@Sueui-MacBookPro.local>
2025-07-02 19:50:08 +0900

    기본 layout 설정 및 랜딩페이지 초기화

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Container from '@/components/container/PageContainer';
+import React from 'react'
+
+export default function LandingPage() {
+  return (
+    <Container>
+        <div className="border">랜딩페이지</div>
+    </Container>
+  )
+}
---
+++ src/components/layout/Gnb.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react'
+
+export default function Gnb() {
+  return (
+    <header className="flex h-14 items-center justify-center border-b-1 border-line-100 bg-white px-6">
+      <div className="flex w-full max-w-[var(--container-xl)] items-center justify-between border">
+      GNB영역
+      </div>
+    </header>
+  )
+}
