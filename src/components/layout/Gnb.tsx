9c116a63251955e63c45c0c1a7f512b0ffaf16c9
Sue <sue@Sueui-MacBookPro.local>
2025-07-02 21:08:38 +0900

    gnb 일부 제작

--- src/components/layout/Gnb.tsx
+++ src/components/layout/Gnb.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import React from 'react'
+import Logo from './_components/Logo';
 
 export default function Gnb() {
   return (
-    <header className="flex h-14 items-center justify-center border-b-1 border-line-100 bg-white px-6">
-      <div className="flex w-full max-w-[var(--container-xl)] items-center justify-between border">
-        GNB영역
+    <header className="flex h-14 md:h-22 px-6  items-center justify-center border-b-1 border-line-100 bg-white ">
+      <div className="flex w-full max-w-[var(--container-xl)] items-center justify-between">
+        <Logo />
       </div>
     </header>
   )
