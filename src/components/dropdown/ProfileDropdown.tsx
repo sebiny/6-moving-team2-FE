e6df8a97d3d78768f3a121db86ae462c51e0acee
Sue <sue@Sueui-MacBookPro.local>
2025-07-07 16:01:55 +0900

    feature: profile dropdown 제작

--- src/components/dropdown/ProfileDropdown.tsx
+++ src/components/dropdown/ProfileDropdown.tsx
@@ -2,13 +2,22 @@ import Image from "next/image";
 import React from "react";
 import icProfile from "/public/assets/icons/ic_profile.svg";
 
+import { useRouter } from "next/navigation";
+
 interface ProfileProps {
   md?: string;
   isOpen: boolean;
   onToggle: () => void;
 }
 
+export const MYPAGE_CUSTOMER = [
+  { label: "프로필 수정", path: "./" },
+  { label: "찜한 기사님", path: "./" },
+  { label: "이사 리뷰", path: "./" }
+];
+
 export default function Profile({ isOpen, onToggle, md }: ProfileProps) {
+  const router = useRouter();
   return (
     <div className="flex items-center">
       {md ? (
@@ -24,8 +33,22 @@ export default function Profile({ isOpen, onToggle, md }: ProfileProps) {
 
       {/* 프로필 레이어 */}
       {isOpen && (
-        <div className="border-line-200 absolute top-12 z-99 flex h-48 w-28 -translate-x-13 flex-col rounded-2xl border bg-gray-50 p-4 shadow-gray-300 md:top-18 md:translate-x-0">
-          드롭다운 제작중
+        <div className="text-black-400 border-line-200 absolute top-12 z-99 flex w-38 -translate-x-1/2 flex-col rounded-2xl border bg-gray-50 pt-4 pb-1 shadow-gray-300 md:top-18 md:w-62 md:-translate-x-1/2 xl:translate-x-0">
+          <button className="h-10 items-center pl-4 text-left text-base font-bold md:h-13 md:pl-6 md:text-lg">
+            사용자이름 고객님
+          </button>
+          {MYPAGE_CUSTOMER.map(({ label, path }, idx) => (
+            <button
+              key={idx}
+              onClick={() => router.push(path)}
+              className="h-10 cursor-pointer items-center pl-4 text-left text-sm hover:bg-orange-50 md:h-13 md:pl-6 md:text-base"
+            >
+              {label}
+            </button>
+          ))}
+          <button className="border-line-100 mt-1 h-10 cursor-pointer border-t text-xs text-gray-500 md:h-13 md:text-sm">
+            로그아웃
+          </button>
         </div>
       )}
     </div>
