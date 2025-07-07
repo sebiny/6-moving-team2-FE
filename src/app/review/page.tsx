1c057e1379f56f4927badfd6ea231d4f972c8274
sebiny <rucy8888@naver.com>
2025-07-07 13:06:44 +0900

    feat: review UI main 부분 제작 중

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -2,11 +2,13 @@ import InputText from "@/components/InputText";
 import Header from "@/app/review/component/Header";
 import React from "react";
 import Main from "./component/Main";
+import Gnb from "@/components/layout/Gnb";
 
 function page() {
   return (
     <div>
       <div>
+        <Gnb />
         {/* <InputText /> */}
         <Header />
         <Main />
--- src/components/layout/Gnb.tsx
+++ src/components/layout/Gnb.tsx
@@ -1,51 +1,46 @@
 "use client";
 
-import Logo from './_components/Logo';
-import GnbList from '@/components/list/GnbList';
-import React, { useEffect, useState } from 'react'
-import Notification from '../dropdown/NotificationDropdown';
-import Profile from '../dropdown/ProfileDropdown';
+import Logo from "./_components/Logo";
+import GnbList from "@/components/list/GnbList";
+import React, { useEffect, useState } from "react";
+import Notification from "../dropdown/NotificationDropdown";
+import Profile from "../dropdown/ProfileDropdown";
 
 export default function Gnb() {
-  const [isMd, setIsMd] = useState(false)
-  
+  const [isMd, setIsMd] = useState(false);
 
-  useEffect(()=>{
-      const handleResize = () => {
-          setIsMd(window.innerWidth >= 744);
-      };
-      handleResize();
-      window.addEventListener('resize', handleResize);
-      return() => window.removeEventListener('resize', handleResize);
-  }, [])
-
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMd(window.innerWidth >= 744);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  //744이상이될때 데스크탑이 되고 있다.
   return (
-    <header className="flex h-14 md:h-22 px-6 items-center justify-center border-b-1 border-line-100 bg-white ">
+    <header className="border-line-100 flex h-14 items-center justify-center border-b-1 bg-white px-6 md:h-22">
       <div className="flex w-full max-w-[var(--container-gnb)] items-center justify-between">
         <Logo />
-        
-          {isMd ?
-            (
-              <div className="flex justify-between items-center flex-1">
-                <GnbList md="md" className="flex-1"/>
-                <div className='flex justify-between items-center gap-5'>
-                  <Notification />
-                  <Profile md="md" />
-                </div>
-              </div>
-            ) :
-            (
-              <div className="flex justify-between items-center gap-5">
-                <Notification />
-                <div className='flex justify-between items-center gap-5'>
-                  <Profile />
-                  <GnbList />
-                </div>
-              </div>
-            )
-          }
-        
+
+        {isMd ? (
+          <div className="flex flex-1 items-center justify-between">
+            <GnbList md="md" className="flex-1" />
+            <div className="flex items-center justify-between gap-5">
+              <Notification />
+              <Profile md="md" />
+            </div>
+          </div>
+        ) : (
+          <div className="flex items-center justify-between gap-5">
+            <Notification />
+            <div className="flex items-center justify-between gap-5">
+              <Profile />
+              <GnbList />
+            </div>
+          </div>
+        )}
       </div>
     </header>
-  )
+  );
 }
