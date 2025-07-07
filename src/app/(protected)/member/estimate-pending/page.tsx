0d48d67f48512c2f3ecb648a27116c2f251a6b77
fs6-kde <kde040508@gmail.com>
2025-07-07 14:38:18 +0900

    대기중인 견적 페이지 작업 중 버셀 배포 해결 위한 1차 pr

--- src/app/(protected)/member/estimate-pending/page.tsx
+++ src/app/(protected)/member/estimate-pending/page.tsx
@@ -1,14 +1,26 @@
 "use client";
 
-import PendingCard from "@/app/(protected)/member/estimate-pending/_components/PendingCard";
-import { pendingData } from "@/app/(protected)/member/estimate-pending/_components/PendingData";
+import EstimateSubHeader from "./_components/EstimateSubHeader";
+import PendingCard from "./_components/PendingCard";
+import { pendingData } from "./_components/PendingData";
+
+const mockEstimateInfo = {
+  label: "소형이사",
+  requestDate: "2024년 6월 24일",
+  from: "서울시 중구",
+  to: "경기도 수원시",
+  date: "2024년 07월 01일 (월)"
+};
 
 export default function ExamplePendingPage() {
   return (
-    <div className="grid gap-6 bg-white px-100 py-50 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
-      {pendingData.map((item) => (
-        <PendingCard key={item.id} data={item} />
-      ))}
-    </div>
+    <>
+      {/* <EstimateSubHeader data={mockEstimateInfo} /> */}
+      <div className="bg-background-200 grid gap-8 py-50 sm:grid-cols-1 sm:px-5 md:grid-cols-1 md:px-15 md:py-30 lg:grid-cols-2 lg:px-100">
+        {pendingData.map((item) => (
+          <PendingCard key={item.id} data={item} />
+        ))}
+      </div>
+    </>
   );
 }
