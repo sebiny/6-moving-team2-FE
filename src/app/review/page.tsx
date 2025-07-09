f80d2aef10ce1be82d7a2d19b43d5a228954ab78
Sue <sue@Sueui-MacBookPro.local>
2025-07-09 14:19:00 +0900

    fix: gnb 사용 영역 코드 삭제

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -9,7 +9,6 @@ function Page() {
   const [selectedIdx, setSelectedIdx] = useState("1");
   return (
     <div>
-      <Gnb userRole="guest" />
       <Header type="review" selectedIdx={selectedIdx} setSelectedIdx={setSelectedIdx} />
       <Main selectedIdx={selectedIdx} />
       <ReviewModal />
