9d25b467636e8b5fd7002575da8d0ae871a14335
Sue <sue@Sueui-MacBookPro.local>
2025-07-11 14:52:26 +0900

    fix: 로그인 여부에 따른 분기처리 영역 ui 수정

--- src/components/dropdown/ProfileDropdown.tsx
+++ src/components/dropdown/ProfileDropdown.tsx
@@ -4,6 +4,7 @@ import icProfile from "/public/assets/icons/ic_profile.svg";
 
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/providers/AuthProvider";
+import { authService } from "@/lib/api/api-auth";
 
 interface ProfileProps {
   lg?: string;
@@ -14,14 +15,20 @@ interface ProfileProps {
 }
 
 export const MYPAGE_CUSTOMER = [
-  { label: "프로필 수정", path: "./" },
-  { label: "찜한 기사님", path: "./" },
-  { label: "이사 리뷰", path: "./" }
+  { label: "프로필 수정", path: "/" },
+  { label: "찜한 기사님", path: "/" },
+  { label: "이사 리뷰", path: "/review" }
 ];
+
+const API_PATH = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export default function Profile({ ref, isOpen, onClick, className, lg }: ProfileProps) {
   const router = useRouter();
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
+
+  const handleLogout = async () => {
+    await logout();
+  };
   return (
     <div className={`${className} flex items-center`} ref={ref}>
       {lg ? (
@@ -39,7 +46,7 @@ export default function Profile({ ref, isOpen, onClick, className, lg }: Profile
       {isOpen && (
         <div className="text-black-400 border-line-200 absolute top-12 z-99 flex w-38 -translate-x-1/2 flex-col rounded-2xl border bg-gray-50 pt-4 pb-1 shadow-gray-300 lg:top-18 lg:w-62 lg:-translate-x-36 xl:translate-x-0">
           <button className="h-10 items-center pl-4 text-left text-base font-bold lg:h-13 lg:pl-6 lg:text-lg">
-            {user?.name ? user?.name : "이름없음"} 고객님
+            {user?.name ?? "이름없음"} 고객님
           </button>
           {MYPAGE_CUSTOMER.map(({ label, path }, idx) => (
             <button
@@ -50,7 +57,10 @@ export default function Profile({ ref, isOpen, onClick, className, lg }: Profile
               {label}
             </button>
           ))}
-          <button className="border-line-100 mt-1 h-10 cursor-pointer border-t text-xs text-gray-500 lg:h-13 lg:text-sm">
+          <button
+            onClick={handleLogout}
+            className="border-line-100 mt-1 h-10 cursor-pointer border-t text-xs text-gray-500 lg:h-13 lg:text-sm"
+          >
             로그아웃
           </button>
         </div>
--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -3,12 +3,12 @@ export const GNB_ROUTE_USER_ROLE = {
     { label: "기사님 찾기", path: "/drivers" },
     { label: "로그인", path: "/customer/login" }
   ],
-  customer: [
+  CUSTOMER: [
     { label: "견적 요청", path: "/" },
     { label: "기사님 찾기", path: "/drivers" },
     { label: "내 견적 관리", path: "/member/my-estimates" }
   ],
-  driver: [
+  DRIVER: [
     { label: "받은 요청", path: "/driver/received-requests" },
     { label: "내 견적 관리", path: "/driver/my-page" }
   ]
