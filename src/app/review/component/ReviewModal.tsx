e0b326a65501cb010c6a1b180870c05bdbf24ad4
sebiny <rucy8888@naver.com>
2025-07-12 10:20:01 +0900

    현재상황저장

--- src/app/review/component/ReviewModal.tsx
+++ src/app/review/component/ReviewModal.tsx
@@ -12,8 +12,8 @@ export default function ReviewModal({ setIsModal }: Props) {
   const [isValid, setIsValid] = useState(false);
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-      <div className="mb-20 h-194 w-150 rounded-[32px] bg-gray-50 p-8 shadow-[4px_4px_10px_rgba(169,169,169,0.2)]">
-        <div className="flex flex-col gap-10">
+      <div className="mb-20 h-[678px] w-[375px] rounded-[32px] bg-gray-50 px-6 py-8 shadow-[4px_4px_10px_rgba(169,169,169,0.2)] lg:h-194 lg:w-150 lg:p-8">
+        <div className="flex flex-col gap-10 gap-[26px]">
           <div className="flex justify-between">
             <p className="text-black-400 text-[24px] leading-[32px] font-semibold">리뷰 쓰기</p>
             <Image
