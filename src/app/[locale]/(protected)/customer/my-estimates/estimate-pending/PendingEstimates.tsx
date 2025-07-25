60b5bdf2d7708f351acc64512113b96941c1b762
fs6-kde <kde040508@gmail.com>
2025-07-25 14:24:07 +0900

    [김다은] 찜한 기사님 데이터 연결 + 전체적인 디테일 점검

--- src/app/[locale]/(protected)/customer/my-estimates/estimate-pending/PendingEstimates.tsx
+++ src/app/[locale]/(protected)/customer/my-estimates/estimate-pending/PendingEstimates.tsx
@@ -10,16 +10,21 @@ import { formatAddress } from "@/utills/addressUtils";
 export default function PendingEstimates() {
   const { data, isLoading } = usePendingEstimates();
 
-  if (isLoading || !data) return <div className="mt-20 flex justify-center">로딩 중...</div>;
+  if (isLoading || !data) return <div className="mt-120 flex justify-center">로딩 중...</div>;
 
   const estimates = data?.estimates ?? []; // 견적서를 바로 못받을 수 있으니 처음은 빈배열
   const estimateRequest = data?.estimateRequest;
 
-  // 견적 요청 작성 전, 대기 중인 견적 페이지를 어떻게 할지 생각해보기
+  // 견적 요청이 없는 경우 → 다른 이미지 + 안내 문구
   if (!estimateRequest) {
     return (
-      <div className="mt-20 flex justify-center">
-        <p className="text-gray-400">작성한 견적 요청이 없습니다.</p>
+      <div className="mt-45 flex flex-col items-center justify-center gap-2 lg:mt-75">
+        <img src="/assets/images/img_empty_car.svg" alt="견적요청 없음" width={250} height={250} className="mr-7" />
+        <p className="text-center text-lg font-normal text-neutral-400">
+          아직 견적 요청 안하셨나요?
+          <br />
+          견적 요청 먼저 작성해주세요!
+        </p>
       </div>
     );
   }
@@ -35,11 +40,18 @@ export default function PendingEstimates() {
 
   return (
     <div className="mt-11 md:mt-13 lg:mt-21">
+      {/* 견적 요청이 있을 경우에만 헤더 렌더링 */}
       <EstimateSubHeader data={requestData} />
+
       <div className="bg-background-200 grid grid-cols-1 gap-8 px-5 py-10 md:grid-cols-1 md:px-15 lg:grid-cols-2 lg:px-100 lg:py-20">
-        {/* 피그마 보고 이미지 추가 */}
         {estimates.length === 0 ? (
-          <p className="flex justify-center text-gray-400 lg:ml-120">아직 받은 견적이 없습니다.</p>
+          <div className="col-span-full flex flex-col items-center justify-center gap-2">
+            <img src="/assets/images/img_moving_car1.svg" alt="견적 없음" width={250} height={250} />
+            <p className="text-center text-lg font-normal text-neutral-400">
+              기사님들이 열심히 확인 중이에요
+              <br />곧 견적이 도착할 거예요!
+            </p>
+          </div>
         ) : (
           estimates.map((estimate) => (
             <PendingCard key={estimate.id} data={estimate} moveType={estimateRequest.moveType} />
