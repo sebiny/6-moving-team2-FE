4c4ed0a5ea7e8d94de670a905b984fb56a912796
sebiny <rucy8888@naver.com>
2025-07-27 00:34:24 +0900

    feat: 리뷰 생성, 내가쓴 리뷰 불러오기 삭제등api제작

--- src/app/[locale]/(protected)/customer/review/_components/ReviewWrite.tsx
+++ src/app/[locale]/(protected)/customer/review/_components/ReviewWrite.tsx
@@ -6,36 +6,49 @@ import { useTranslations } from "next-intl";
 
 interface Props {
   setIsValid: (value: boolean) => void;
+  setRating: (value: number) => void;
+  setContent: (value: string) => void;
 }
 
-export default function ReviewWrite({ setIsValid }: Props) {
+export default function ReviewWrite({ setIsValid, setRating, setContent }: Props) {
   const t = useTranslations("Review");
   const { isLg, isSm } = useMediaHook();
   const [InputValid, setInputValid] = useState(false);
-  const [rating, setRating] = useState(0);
+  const [localRating, setLocalRating] = useState(0);
+  const [localContent, setLocalContent] = useState("");
+
   useEffect(() => {
-    if (rating > 0 && InputValid) {
-      setIsValid(true);
+    setIsValid(localRating > 0 && InputValid);
+
+    if (localContent) {
+      setContent(localContent);
     }
-  }, [rating, InputValid]);
+  }, [localRating, InputValid]);
+
+  const handleRating = (num: number) => {
+    setLocalRating(num);
+    setRating(num); // 부모로 전달
+  };
+
   const textClass = "text-black-300 mb-3 lg:text-[18px] text-[16px] leading-[26px] font-semibold ";
+
   return (
     <div className="flex flex-col gap-7 lg:gap-8">
       <div>
         <p className={textClass}>{t("modal.rate")}</p>
         <div className="relative h-6 w-30 lg:h-9 lg:w-45">
-          {isSm && !isLg && <StarIcon width={120} height={24} rating={rating} />}
-          {isLg && <StarIcon width={180} height={36} rating={rating} />}
+          {isSm && !isLg && <StarIcon width={120} height={24} rating={localRating} />}
+          {isLg && <StarIcon width={180} height={36} rating={localRating} />}
           <div className="absolute top-0 left-0 flex">
             {[1, 2, 3, 4, 5].map((num) => (
-              <div key={num} className="h-6 w-6 cursor-pointer lg:h-9 lg:w-9" onClick={() => setRating(num)} />
+              <div key={num} className="h-6 w-6 cursor-pointer lg:h-9 lg:w-9" onClick={() => handleRating(num)} />
             ))}
           </div>
         </div>
       </div>
       <div>
         <p className={textClass}>{t("modal.detail")}</p>
-        <InputText setInputValid={setInputValid} />
+        <InputText value={localContent} setInputValid={setInputValid} onChange={setLocalContent} />
       </div>
     </div>
   );
