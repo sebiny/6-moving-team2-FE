545b55296a73fc2bae6955f3233539bd9e07a583
Sue <sue@Sueui-MacBookPro.local>
2025-08-08 09:41:59 +0900

    chor: 랜딩페이지 최적화

--- src/app/[locale]/landing/_component/ChangeImages.tsx
+++ src/app/[locale]/landing/_component/ChangeImages.tsx
@@ -1,9 +1,27 @@
 import React, { useEffect, useState } from "react";
-import ImgEstimateVisualDefault from "/public/assets/images/img_landing_card.png";
-import ImgEstimateVisualMd from "/public/assets/images/img_landing_card_md.png";
-import ImgEstimateVisualLg from "/public/assets/images/img_landing_card_lg.png";
+import ImgEstimateVisualDefault from "/public/assets/images/img_landing_card.avif";
+import ImgEstimateVisualMd from "/public/assets/images/img_landing_card_md.avif";
+import ImgEstimateVisualLg from "/public/assets/images/img_landing_card_lg.avif";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
+
+const IMG_ESTIMATE = {
+  default: {
+    src: ImgEstimateVisualDefault,
+    width: 375,
+    height: 496
+  },
+  md: {
+    src: ImgEstimateVisualMd,
+    width: 680,
+    height: 835
+  },
+  lg: {
+    src: ImgEstimateVisualLg,
+    width: 1400,
+    height: 787
+  }
+};
 
 export default function ChangeImages() {
   const [windowWidth, setWindowWidth] = useState(0);
@@ -18,16 +36,31 @@ export default function ChangeImages() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  let estimateVisualImg = ImgEstimateVisualDefault;
+  let estimateVisualSrc = IMG_ESTIMATE.default.src;
+  let estimateVisualWidth = IMG_ESTIMATE.default.width;
+  let estimateVisualHeight = IMG_ESTIMATE.default.height;
   if (windowWidth >= 1280) {
-    estimateVisualImg = ImgEstimateVisualLg;
+    estimateVisualSrc = IMG_ESTIMATE.lg.src;
+    estimateVisualWidth = IMG_ESTIMATE.lg.width;
+    estimateVisualHeight = IMG_ESTIMATE.lg.height;
   } else if (windowWidth >= 375) {
-    estimateVisualImg = ImgEstimateVisualMd;
+    estimateVisualSrc = IMG_ESTIMATE.md.src;
+    estimateVisualWidth = IMG_ESTIMATE.md.width;
+    estimateVisualHeight = IMG_ESTIMATE.md.height;
   }
 
   return (
     <div className="relative flex w-full lg:justify-end">
-      <Image src={estimateVisualImg} alt="견적카드" />
+      <Image
+        src={estimateVisualSrc}
+        width={estimateVisualWidth}
+        height={estimateVisualHeight}
+        alt="견적카드"
+        className="h-auto w-full object-contain"
+        priority
+        quality={85}
+        unoptimized // lazyloading eagerloading, 클라우드 프론트 거칠 경우 2회차부터 속도 빨라짐
+      />
       <div className="absolute top-8 z-50 w-full pr-10 text-right text-xl font-bold text-gray-50 md:top-13 md:text-[32px] lg:top-54 lg:flex lg:w-1/2 lg:justify-start lg:pl-10 lg:text-[32px]">
         <div className="lg:text-left">
           {t("imageTitle")}
--- src/app/[locale]/landing/_component/MovingCard.tsx
+++ src/app/[locale]/landing/_component/MovingCard.tsx
@@ -26,7 +26,14 @@ export default function MovingCard({ type }: { type: "small" | "family" | "offic
   };
   return (
     <div className="bg-background-200 flex min-h-31 min-w-31 flex-col items-center justify-center rounded-[20px] border-orange-300 transition-all delay-100 duration-300 ease-in-out hover:scale-120 hover:border-2 sm:min-h-50 sm:min-w-50 sm:hover:border-3">
-      <Image src={MOVING_TYPE[type].src} alt="소형이사" width={52} height={52} className="h-14 w-14 sm:h-25 sm:w-25" />
+      <Image
+        src={MOVING_TYPE[type].src}
+        alt="소형이사"
+        width={52}
+        height={52}
+        className="h-14 w-14 sm:h-25 sm:w-25"
+        unoptimized
+      />
       <div className="mt-2 flex flex-col text-center sm:mt-4">
         <span className="text-black-500 text-[10px] font-bold sm:text-base">{MOVING_TYPE[type].titleText}</span>
         <span className="text-[8px] font-medium text-gray-500 sm:text-[10px]">{MOVING_TYPE[type].subText}</span>
