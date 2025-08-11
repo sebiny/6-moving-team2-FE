e71d8b47790985eda36e124495ec67c51f61064e
fs6-kde <kde040508@gmail.com>
2025-08-11 13:07:32 +0900

    [김다은] 푸쉬했는데 안올라가서 한번 더 커밋

--- src/app/[locale]/(protected)/customer/my-estimates/MyEstimate.tsx
+++ src/app/[locale]/(protected)/customer/my-estimates/MyEstimate.tsx
@@ -42,7 +42,6 @@ export default function MyEstimatePage() {
         className="fixed z-9 w-full bg-white shadow-[inset_0_-1px_0_rgba(0,0,0,0.08)] lg:top-22"
         aria-label="내 견적 관리 상단"
       >
-        {/* 헤더 컴포넌트가 role="tablist" 등을 제공하도록 아래 참고사항대로 보강 권장 */}
         <Header type="estimate" selectedIdx={selectedIdx} setSelectedIdx={handleTabChange} />
       </header>
 
