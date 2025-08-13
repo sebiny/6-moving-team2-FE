2106eeb5d297300810ebcadfa368964caabb4e98
elisaohh <elisaohh@gmail.com>
2025-08-13 17:15:33 +0900

    fix: aria-label 오류 수정

--- src/app/[locale]/(protected)/customer/estimate-request/_components/modal/DaumPostcodeModal.tsx
+++ src/app/[locale]/(protected)/customer/estimate-request/_components/modal/DaumPostcodeModal.tsx
@@ -40,7 +40,7 @@ export default function DaumPostcodeModal({ onComplete, onClose, query }: Postco
       <div className="w-[608px] max-w-lg overflow-hidden rounded-2xl bg-white shadow-xl">
         {/* 상단 헤더 */}
         <header className="flex items-center gap-2 border-gray-100 p-4">
-          <button onClick={onClose} aria-label={t("back", { default: "뒤로가기" })}>
+          <button onClick={onClose} aria-label={"뒤로가기"}>
             <Image
               src="/assets/icons/ic_chevron_left_black.svg"
               alt=""
@@ -66,6 +66,7 @@ export default function DaumPostcodeModal({ onComplete, onClose, query }: Postco
               width: "100%",
               height: "400px"
             }}
+            focusInput={false}
           />
         </div>
       </div>
